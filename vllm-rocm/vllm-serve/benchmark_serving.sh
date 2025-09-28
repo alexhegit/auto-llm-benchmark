bb17e1d832d866cdd710f0fc793ec049d3b63a91
alexhegit <31022192+alexhegit@users.noreply.github.com>
2025-09-28 19:24:05 +0800

    Update benchmark_serving.sh

--- vllm-rocm/vllm-serve/benchmark_serving.sh
+++ vllm-rocm/vllm-serve/benchmark_serving.sh
@@ -1,22 +1,25 @@
 #!/bin/sh
 
 #wget https://huggingface.co/datasets/anon8231489123/ShareGPT_Vicuna_unfiltered/resolve/main/ShareGPT_V3_unfiltered_cleaned_split.json
-
-BM_S=/app/vllm/benchmarks/benchmark_serving.py
-MODEL=/data/llm/Meta-Llama-3.1-8B/
+#!/bin/sh
+#BM_S=/app/vllm/benchmarks/benchmark_serving.py
+MODEL=Llama-3.3-70B-Instruct
 DATASET=./ShareGPT_V3_unfiltered_cleaned_split.json
 
-python3 $BM_S --backend vllm \
-	--model $MODEL \
-	--tokenizer $MODEL \
-	--dataset-name sharegpt \
-	--dataset-path $DATASET \
-	--num-prompts 200 \
-	--port 8000 \
-	--endpoint /v1/completions \
-	--percentile-metrics "ttft,tpot,itl,e2el" \
-	--save-result \
-	2>&1 | tee benchmark_serving.log
+#python3 $BM_S --backend vllm \
+vllm bench serve \
+  --backend openai \
+  --model $MODEL \
+  --tokenizer /data/$MODEL \
+  --dataset-name sharegpt \
+  --dataset-path $DATASET \
+  --num-prompts 200 \
+  --host 10.10.38.14 \
+  --port 31436 \
+  --endpoint /v1/completions \
+  --percentile-metrics "ttft,tpot,itl,e2el" \
+  --save-result \
+  2>&1 | tee benchmark_serving.log
 
 
 echo "### Serving Benchmarks" >> benchmark_results.md
@@ -28,3 +31,4 @@ echo "" >> benchmark_results.md
 echo '```' >> benchmark_results.md
 tail -n 25 benchmark_serving.log >> benchmark_results.md
 echo '```' >> benchmark_results.md
+
